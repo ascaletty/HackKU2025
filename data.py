0c2271aa0fde0eaa6cf220d703b9528e3c32ec15
Aaron Scaletty <ascaletty23@gmail.com>
2025-04-05 16:54:48 +0000

    added preprocessing features to convert to lowercase and remove punc

--- data.py
+++ data.py
@@ -4,7 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import string
 import re
-import time 
+import time
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
 start= time.perf_counter()
 end= time.perf_counter()
 elapsed = start - end
@@ -13,12 +18,21 @@ path = kagglehub.dataset_download("thedevastator/c-ssrs-labeled-suicidality-in-5
 
 print("Path to dataset files:", path)
 data_pre= pd.read_csv('/home/ascaletty23/.cache/kagglehub/datasets/thedevastator/c-ssrs-labeled-suicidality-in-500-anonymized-red/versions/2/500_anonymized_Reddit_users_posts_labels - 500_anonymized_Reddit_users_posts_labels.csv')
-print(data_pre)
 list_labels = data_pre['Label'].unique()
 
 print(list_labels)
-df['Label'] = df['Label'].replace('Supportive', 2)
-df['Label'] = df['Lable'].str.replace('Ideation', 4)
+data_pre['Label'] = data_pre['Label'].replace('Indicator', 0)
+data_pre['Label'] = data_pre['Label'].replace('Supportive', 1)
+data_pre['Label'] = data_pre['Label'].replace('Behavior', 2)
+data_pre['Label'] = data_pre['Label'].replace('Ideation', 3)
+data_pre['Label'] = data_pre['Label'].replace('Attempt', 4)
+data_pre['Post']= data_pre['Post'].str.lower()
+data_pre['Post'].str.replace('[{}]'.format(string.punctuation), '')
+print(data_pre)
+
+
+
+
 
 
 
