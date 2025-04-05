a52f275ded578182f7435897163b3dfd224b81a1
Aaron Scaletty <ascaletty23@gmail.com>
2025-04-05 18:11:38 +0000

    updated algorithm to tokenize

--- datapl.py
+++ datapl.py
@@ -1,5 +1,5 @@
 import kagglehub
-import pandas as pd
+import polars as pl
 import numpy as np
 import matplotlib.pyplot as plt
 import string
@@ -12,7 +12,7 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 
-pd.set_option('future.no_silent_downcasting', True)
+#pl.set_option('future.no_silent_downcasting', True)
 start= time.perf_counter()
 end= time.perf_counter()
 elapsed = start - end
@@ -20,14 +20,14 @@ elapsed = start - end
 path = kagglehub.dataset_download("thedevastator/c-ssrs-labeled-suicidality-in-500-anonymized-red")
 
 print("Path to dataset files:", path)
-df= pd.read_csv('/home/ascaletty23/.cache/kagglehub/datasets/thedevastator/c-ssrs-labeled-suicidality-in-500-anonymized-red/versions/2/500_anonymized_Reddit_users_posts_labels - 500_anonymized_Reddit_users_posts_labels.csv')
+df= pl.read_csv('/home/ascaletty23/.cache/kagglehub/datasets/thedevastator/c-ssrs-labeled-suicidality-in-500-anonymized-red/versions/2/500_anonymized_Reddit_users_posts_labels - 500_anonymized_Reddit_users_posts_labels.csv')
 list_labels = df['Label'].unique()
 #print(df)
-df['Label'] = df['Label'].replace('Indicator', 0)
-df['Label'] = df['Label'].replace('Supportive', 1)
-df['Label'] = df['Label'].replace('Behavior', 2)
-df['Label'] = df['Label'].replace('Ideation', 3)
-df['Label'] = df['Label'].replace('Attempt', 4)
+df['Label'] = df['Label'].replace_strict('Indicator', 0)
+df['Label'] = df['Label'].replace_strict('Supportive', 1)
+df['Label'] = df['Label'].replace_strict('Behavior', 2)
+df['Label'] = df['Label'].replace_strict('Ideation', 3)
+df['Label'] = df['Label'].replace_strict('Attempt', 4)
 df['Post']= df['Post'].str.lower()
 df['Post'] = df['Post'].str.replace(',',' ')
 df['Post'] = df['Post'].str.replace('.',' ')
