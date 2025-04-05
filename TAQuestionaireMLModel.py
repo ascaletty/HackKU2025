d0418ab12ed02be618bc1157612ec4c24eaff5ba
Terry Akers <terrya3141@gmail.com>
2025-04-05 15:37:48 -0500

    terry's commit for the heatmap

--- TAQuestionaireMLModel.py
+++ TAQuestionaireMLModel.py
@@ -5,8 +5,14 @@ depression_data = pd.read_csv(r'c:\Users\terry\OneDrive\Desktop\HackKU25\Preproc
 #I don't know how to update the path to make it work for everyone so for yours just put in your own path, the file is in the github.
 
 import seaborn as sns # I am using seaborn to create the correlation heatmap.
+import matplotlib.pyplot as plt # for the plotting
 numerical_depression_data = depression_data.select_dtypes(include=['int64', 'float64']) 
 #This is a new variable to select only the numerical data types from the depression_data variable.
-# hi
+correlation_matrix = numerical_depression_data.corr()  # Calculate the correlation matrix
 
+# Plot the heatmap
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='cubehelix_r', fmt='.2f')
+plt.title('Correlation Matrix')
+plt.show()
 
