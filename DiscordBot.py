9e96743e64ba034520ca49edfe7dbc90223c609b
Somesthetic <forest-denton@ku.edu>
2025-04-06 00:17:30 -0500

    Discord bot can now dm

--- DiscordBot.py
+++ DiscordBot.py
@@ -5,14 +5,17 @@ Simple Discord bot to handle user input bc I'm too lazy to learn web dev stuff, 
 import os
 import discord
 from dotenv import load_dotenv  # simple library to save me from getting my key doxed
+from discord.ext import commands
 
 
-class DeprosoBot():
+class DepressoBot:
     def __init__(self):
         load_dotenv()  # loads the .env file
         self.TOKEN = str(os.getenv('DISCORD_TOKEN'))  # retrieves the discord token
 
         intents = discord.Intents.default()
+        intents.message_content = True
+        intents.dm_messages = True
         self.client = discord.Client(intents=intents)
         self.start_listening()
 
@@ -25,6 +28,15 @@ class DeprosoBot():
         async def on_message(message):
             if message.author == self.client.user:
                 return
-            await message.channel.send('I LIVE AND BREATH')
+            if 'could i have depression?' in message.content.lower():
+                uer = message.author
+                await uer.send('Sick')
+                await message.channel.send('who knows?')
+
+
 
         self.client.run(self.TOKEN)
+
+
+
+begin_everything = DepressoBot()
