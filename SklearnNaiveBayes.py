8192610090148aefabe2cadff79883e026cad0fd
Aaron Scaletty <ascaletty23@gmail.com>
2025-04-06 01:30:10 +0000

    final commit before the great merge

--- SklearnNaiveBayes.py
+++ SklearnNaiveBayes.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
->>>>>>> Terry
+
 df= pd.read_csv('reprocessed_data.csv')
 df= df.drop('Job Satisfaction',axis=1)
 df= df.drop('Work Pressure',axis=1)
@@ -25,7 +25,7 @@ X= df.drop('11', axis=1)
 print(X)
 y= df['11']
 print(y)
-<<<<<<< HEAD
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
 gnb = GaussianNB()
 y_pred = gnb.fit(X_train, y_train).predict(X_test)
@@ -41,23 +41,16 @@ accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy:{accuracy}")
 import sys
 file_path = sys.argv[1]
-try:
-        with open(file_path, 'r') as file:
-            userdf = pd.read_csv(file_path)
-    except FileNotFoundError:
-        print(f"Error: File not found: {file_path}")
-        sys.exit(1)
-    except Exception as e:
-         print(f"An error occurred: {e}")
-         sys.exit(1)
+with open(file_path, 'r') as file:
+    userdf = pd.read_csv(file_path)
 print("user data")
 k=0
 while k < 11:
     l= str(k)
-    ct = ColumnTransformer(
+    pt = ColumnTransformer(
         [("numpreprocess",MinMaxScaler(),[k])]
         )
-    userdf[k] = ct.fit_transform(userdf)
+    userdf[k] = pt.fit_transform(userdf)
     i += 1
 print(userdf)
 
