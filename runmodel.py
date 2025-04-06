12cba800de0b6d09b91e8637d137231298cd634a
Somesthetic <forest-denton@ku.edu>
2025-04-06 04:04:21 -0500

    BotQuestionare now works flawlessly!

--- runmodel.py
+++ runmodel.py
@@ -2,7 +2,7 @@ import pickle as pk
 import pandas as pd
 def runmodel(file_name):
     model= pk.load(open('models/naive_bayes.pkl', 'rb'))
-    df= pd.read_csv("questionaredata/"+ file_name)
+    df= pd.read_csv(file_name)
     df.columns =["0","1","2","3","4","5","6","7","8","9","10","11"]
     userdf= df.drop('11', axis=1)
     depression_prediction_raw= model.predict(userdf)
@@ -15,5 +15,3 @@ def runmodel(file_name):
         depression_prediction = False
         print(depression_prediction)
         return depression_prediction
-file_name='test.csv'
-runmodel(file_name)
